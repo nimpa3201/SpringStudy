690c7ab55903733f01171d504b2439cace580605
권길남 <nimpa3201@naver.com>
2024-01-18 02:37:28 +0900

    Test: 주문과 할인 도메인 실행과 테스트

---
+++ core/src/main/java/com/example/core/OrderApp.java
@@ -0,0 +1,27 @@
+package com.example.core;
+
+import com.example.core.member.Grade;
+import com.example.core.member.Member;
+import com.example.core.member.MemberService;
+import com.example.core.member.MemberServiceImpl;
+import com.example.core.order.Order;
+import com.example.core.order.OrderService;
+import com.example.core.order.OrderServiceImpl;
+
+public class OrderApp {
+    public static void main(String[] args) {
+        MemberService memberService = new MemberServiceImpl();
+        OrderService orderService = new OrderServiceImpl();
+
+        Long memberId =1L;
+        Member member = new Member(memberId,"memberA", Grade.VIP);
+        memberService.join(member); // 멤버 추가
+
+        Order order = orderService.createOrder(memberId,"itemA",10000);
+
+        System.out.println(order);
+        System.out.println(order.calculate());
+
+
+    }
+}
