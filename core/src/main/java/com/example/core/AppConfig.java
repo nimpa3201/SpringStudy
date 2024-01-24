cb1b8ef9073b1f8fc8af22b44ebc3fb2e3c8dfe2
권길남 <nimpa3201@naver.com>
2024-01-24 23:20:31 +0900

    Refactor: AppConfig 중복제거

--- core/src/main/java/com/example/core/AppConfig.java
+++ core/src/main/java/com/example/core/AppConfig.java
@@ -1,6 +1,8 @@
 package com.example.core;
 
+import com.example.core.discount.DiscountPolicy;
 import com.example.core.discount.FixDiscountPolicy;
+import com.example.core.member.MemberRepository;
 import com.example.core.member.MemberService;
 import com.example.core.member.MemberServiceImpl;
 import com.example.core.member.MemoryMemberRepository;
@@ -10,10 +12,21 @@ import com.example.core.order.OrderServiceImpl;
 public class AppConfig {
 
     public MemberService memberService(){
-        return new MemberServiceImpl(new MemoryMemberRepository());
+        return new MemberServiceImpl(memberRepository());
+    }
+
+    private MemberRepository memberRepository(){
+        return new MemoryMemberRepository();
     }
 
     public OrderService orderService(){
-        return new OrderServiceImpl(new MemoryMemberRepository(),new FixDiscountPolicy());
+        return new OrderServiceImpl(memberRepository(),discountPolicy());
+    }
+
+    public DiscountPolicy discountPolicy(){
+        return new FixDiscountPolicy();
     }
 }
+
+// new MemoryMemberRepository() 부분 중복이 제거 되었다. MemoryMemberRepository를 다른 구현체로 변경할 때 한 부분만 변경하면 된다
+// AppConfig 역활과 구현 클래가 하눈에 들어온다. 애플리케이션 전체 구성이 어떻게 되어있는지 빠르게 파악할 수 있다.
