7ef0bb8e32b3fc23332ecf951c15edcf43527bb8
권길남 <nimpa3201@naver.com>
2024-02-10 19:50:06 +0900

    Feat: 스프링 빈 조회

---
+++ core/src/test/java/com/example/core/beanfind/ApplicationContextBasicFindTest.java
@@ -0,0 +1,51 @@
+package com.example.core.beanfind;
+
+import com.example.core.AppConfig;
+import com.example.core.member.MemberService;
+import com.example.core.member.MemberServiceImpl;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.NoSuchBeanDefinitionException;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
+import static org.assertj.core.api.Assertions.*;
+
+public class ApplicationContextBasicFindTest {
+    AnnotationConfigApplicationContext ac = new AnnotationConfigApplicationContext(AppConfig.class);
+    @Test
+    @DisplayName("빈 이름으로 조회하기")
+    void findBeanByName(){
+        MemberService memberService = ac.getBean("memberService", MemberService.class);
+        //검증
+        //System.out.println("memberService = "+ memberService);
+        //System.out.println("memberService.getClass() =" + memberService.getClass());
+        assertThat(memberService).isInstanceOf(MemberServiceImpl.class);
+    }
+
+    @Test
+    @DisplayName("이름없이 타입으로 조회")
+    void findBeanByType() {
+        MemberService memberService = ac.getBean(MemberService.class);
+        assertThat(memberService).isInstanceOf(MemberServiceImpl.class);
+
+    }
+
+    @Test
+    @DisplayName("구체 타입으로 조회") //유연성이 떨어짐
+    void findBeanByName2(){
+        MemberServiceImpl memberService = ac.getBean("memberService", MemberServiceImpl.class);
+        assertThat(memberService).isInstanceOf(MemberServiceImpl.class);
+    }
+
+    @Test
+    @DisplayName("빈 이름으로 조회X")
+    void findBeanByNameX(){
+        //MemberService xxxx = ac.getBean("xxxx", MemberService.class);
+
+        org.junit.jupiter.api.Assertions.assertThrows(NoSuchBeanDefinitionException.class,
+                ()-> ac.getBean("xxxx", MemberService.class));
+    }
+
+
+    }
