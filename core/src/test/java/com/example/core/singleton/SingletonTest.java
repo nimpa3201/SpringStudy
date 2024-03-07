72dcc35b4b957e1d2a708d8a44070825fb03c05a
권길남 <nimpa3201@naver.com>
2024-03-07 12:40:24 +0900

    Add: 싱글톤 테스트

--- core/src/test/java/com/example/core/singleton/SingletonTest.java
+++ core/src/test/java/com/example/core/singleton/SingletonTest.java
@@ -6,6 +6,8 @@ import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
+import static org.assertj.core.api.Assertions.*;
+
 public class SingletonTest {
     @Test
     @DisplayName("스프링이 없는 순수한 DI 컨테이너")
@@ -21,6 +23,21 @@ public class SingletonTest {
         System.out.println("memberService2 = " + memberService2);
 
         // memberservice1 != memberservice2
-        Assertions.assertThat(memberService1).isNotSameAs(memberService2);
+        assertThat(memberService1).isNotSameAs(memberService2);
     }
+
+    @Test
+    @DisplayName("싱글톤 패턴을 적용한 객체 사용")
+    void singletonServiceTest(){
+        SingletonService singletonservice1 = SingletonService.getInstance();
+        SingletonService singletonservice2 = SingletonService.getInstance();
+
+        System.out.println("singletonservice1 = " + singletonservice1);
+        System.out.println("singletonservice2 = " + singletonservice2);
+
+        assertThat(singletonservice1).isSameAs(singletonservice2);
+
+
+    }
+
 }
