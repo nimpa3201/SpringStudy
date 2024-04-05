c20d31ca1cc0e44cb110b19a7acc86ecbd6fffef
권길남 <nimpa3201@naver.com>
2024-04-05 17:35:04 +0900

    Update: AutoAppConfig의 컴포넌트 스캔 설정 수정

--- core/src/main/java/com/example/core/AutoAppConfig.java
+++ core/src/main/java/com/example/core/AutoAppConfig.java
@@ -6,6 +6,8 @@ import org.springframework.context.annotation.FilterType;
 
 @Configuration
 @ComponentScan(
+        basePackageClasses= AutoAppConfig.class,
+        //basePackages = "com.example.core.member",
         excludeFilters = @ComponentScan.Filter(type = FilterType.ANNOTATION, classes = Configuration.class)
         //이 필터는 @Configuration이 등록된 AppConfig등의 설정 정보 등록과 실행을 막는다
 ) // 스프링빈 자동으로 끌어올림
