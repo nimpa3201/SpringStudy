ce17d1dc40a111a76a14e7ab34cd9c1c13479ea5
권길남 <nimpa3201@naver.com>
2024-04-18 15:39:02 +0900

    refactor: @Autowired 생성자 제거 및 @RequiredArgsConstructor 애너테이션 적용

--- core/src/main/java/com/example/core/order/OrderServiceImpl.java
+++ core/src/main/java/com/example/core/order/OrderServiceImpl.java
@@ -6,20 +6,23 @@ import com.example.core.member.Member;
 import com.example.core.member.MemberRepository;
 import com.example.core.member.MemberService;
 import com.example.core.member.MemoryMemberRepository;
+import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Component;
 
 @Component
+@RequiredArgsConstructor //final이 붙은 필드값에 대해 생성자를 만들어줌
 public class OrderServiceImpl implements OrderService{
 
     private final MemberRepository memberRepository ;
     private final DiscountPolicy discountPolicy;
-    @Autowired
-    public OrderServiceImpl( @Qualifier("memoryMemberRepository")MemberRepository memberRepository, @Qualifier("rateDiscountPolicy")DiscountPolicy discountPolicy) {
-        this.memberRepository = memberRepository;
-        this.discountPolicy = discountPolicy;
-    }
+
+   /* OrderServiceImpl 클래스에서 @Autowired 생성자를 제거하고, 롬복의 @RequiredArgsConstructor 애너테이션을 추가하여 final 필드에 대한 생성자를 자동으로 생성했다.
+
+    이 변경은 코드를 더 간결하게 만들고, 의존성 주입을 보다 명확하게 관리할 수 있도록 했다.  */
+
+
 
     @Override
     public Order createOrder(Long memberId, String itemName, int itemPrice) {
