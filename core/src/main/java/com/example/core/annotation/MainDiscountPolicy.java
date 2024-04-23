ddbc3264b74b12fd16aecaee1248dc431ec54204
권길남 <nimpa3201@naver.com>
2024-04-23 15:31:03 +0900

    Add: 기본 할인 정책 빈 선택을 위한 사용자 정의 애너테이션@MainDiscountPolicy

---
+++ core/src/main/java/com/example/core/annotation/MainDiscountPolicy.java
@@ -0,0 +1,16 @@
+package com.example.core.annotation;
+
+
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.stereotype.Component;
+
+import java.lang.annotation.*;
+
+@Target({ElementType.FIELD, ElementType.METHOD, ElementType.PARAMETER, ElementType.TYPE, ElementType.ANNOTATION_TYPE})
+@Retention(RetentionPolicy.RUNTIME)
+@Inherited
+@Documented
+@Qualifier("mainDiscountPolicy")
+@Component
+public @interface MainDiscountPolicy  {
+}
