f23b158e6ae372b3a365b8e49d35d26145ce6ce6
권길남 <nimpa3201@naver.com>
2024-05-16 16:30:46 +0900

    Refactor: 프로바이더를 이용한 스프링 프로토타입 스코프 테스트

--- core/src/test/java/com/example/core/scope/PrototypeProviderTest.java
+++ core/src/test/java/com/example/core/scope/PrototypeProviderTest.java
@@ -37,8 +37,7 @@ public class PrototypeProviderTest {
             return prototypeBean.getCount();
         }
     }
-
-    @Component
+    
     @Scope("prototype")
     static class PrototypeBean {
         private int count=0;
