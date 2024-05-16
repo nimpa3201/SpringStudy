d6e309be5d932763fbdb64ec4b5f2e07794c0042
권길남 <nimpa3201@naver.com>
2024-05-16 14:18:50 +0900

    feat: Spring 애플리케이션 컨텍스트에서 프로토타입 스코프 테스트 구현

---
+++ core/src/test/java/com/example/core/scope/PrototypeProviderTest.java
@@ -0,0 +1,63 @@
+package com.example.core.scope;
+
+import jakarta.annotation.PostConstruct;
+import jakarta.annotation.PreDestroy;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.context.annotation.Scope;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class PrototypeProviderTest {
+
+    @Test
+    void providerTest(){
+        AnnotationConfigApplicationContext ac =
+                new AnnotationConfigApplicationContext(PrototypeBean.class, ClientBean.class);
+       ClientBean clientBean1 = ac.getBean(ClientBean.class); //클라이언트 A
+        int count1 = clientBean1.logic();
+        assertThat(count1).isEqualTo(1);
+
+        ClientBean clientBean2 = ac.getBean(ClientBean.class); //클라이언트 B
+        int count2 = clientBean2.logic();
+        assertThat(count2).isEqualTo(1);
+    }
+
+    static class ClientBean{
+        @Autowired
+        private ApplicationContext ac;
+
+        public int logic(){
+            PrototypeBean prototypeBean = ac.getBean(PrototypeBean.class);
+            prototypeBean.addCount();
+            return prototypeBean.getCount();
+        }
+
+    }
+
+    @Scope("prototype")
+    static class PrototypeBean {
+        private int count=0;
+
+        public void addCount(){
+            count++;
+        }
+
+        public int getCount(){
+            return count;
+        }
+
+        @PostConstruct
+        public void init(){
+            System.out.println("prototypeBean.init" + this);
+        }
+
+        @PreDestroy
+        public void destroy(){
+            System.out.println("prototypeBean.destroy");
+        }
+    }
+}
+
