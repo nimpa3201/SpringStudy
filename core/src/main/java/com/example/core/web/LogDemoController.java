e984f5cb9dfff66bf3977b0a033eaf4b586274eb
권길남 <nimpa3201@naver.com>
2024-05-27 19:03:40 +0900

    feat: LogDemoController 추가 및 로그 기능 구현
    - @Controller 및 @RequiredArgsConstructor 어노테이션으로 LogDemoController 클래스 추가
    - LogDemoService 및 ObjectProvider<MyLogger> 의존성 주입
    - log-demo 엔드포인트 추가하여 요청 URL 로그 출력
    - MyLogger 빈을 ObjectProvider를 통해 지연 생성하여 사용
    - logDemoService.logic 호출을 통해 서비스 계층에서의 로직 처리 및 로그 출력 구현

--- core/src/main/java/com/example/core/web/LogDemoController.java
+++ core/src/main/java/com/example/core/web/LogDemoController.java
@@ -1,2 +1,30 @@
-package com.example.core.web;public class LogDemoController {
+package com.example.core.web;
+
+import com.example.core.common.MyLogger;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.ObjectProvider;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+@RequiredArgsConstructor
+public class LogDemoController {
+    private final LogDemoService logDemoService;
+    private final ObjectProvider<MyLogger> myLoggerProvider;
+
+    @RequestMapping("log-demo")
+    @ResponseBody
+    public String logDemo(HttpServletRequest request) throws InterruptedException {
+        String requestURL = request.getRequestURL().toString();
+        MyLogger myLogger = myLoggerProvider.getObject();
+        myLogger.setRequestURL(requestURL);
+
+        myLogger.log("controller test");
+        //Thread.sleep(1000); 각 요청의 로그 순서 섞이지만 수행되는 거 볼 수 있음
+        logDemoService.logic("testId");
+        return "새로운 로그 찍기";
+    }
 }
