89f04299c4010fc69898bbcfb46c504967d04fbb
권길남 <nimpa3201@naver.com>
2024-06-27 16:11:55 +0900

    Feat: View 렌더링을 위한 MyView 클래스 구현

---
+++ servlet/src/main/java/com/example/servlet/web/frontcontroller/MyView.java
@@ -0,0 +1,23 @@
+package com.example.servlet.web.frontcontroller;
+
+import jakarta.servlet.RequestDispatcher;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+import java.io.IOException;
+
+public class MyView {
+    String viewPath;
+
+    public MyView(String urlPath) {
+        this.viewPath = urlPath;
+    }
+
+    public void render(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        RequestDispatcher dispatcher = request.getRequestDispatcher(viewPath);
+        dispatcher.forward(request, response);
+
+    }
+}
+
