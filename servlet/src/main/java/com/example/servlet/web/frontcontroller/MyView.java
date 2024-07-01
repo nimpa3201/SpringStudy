90fe40a00060059bfbb69f5ecac3d7793c0e3ee9
권길남 <nimpa3201@naver.com>
2024-07-01 18:59:10 +0900

    Add: MyView 클래스에 모델 데이터를 요청 속성으로 변환하여 뷰로 전달하는 기능 추가

--- servlet/src/main/java/com/example/servlet/web/frontcontroller/MyView.java
+++ servlet/src/main/java/com/example/servlet/web/frontcontroller/MyView.java
@@ -2,10 +2,12 @@ package com.example.servlet.web.frontcontroller;
 
 import jakarta.servlet.RequestDispatcher;
 import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 
 import java.io.IOException;
+import java.util.Map;
 
 public class MyView {
     String viewPath;
@@ -18,6 +20,18 @@ public class MyView {
         RequestDispatcher dispatcher = request.getRequestDispatcher(viewPath);
         dispatcher.forward(request, response);
 
+    }
+
+    public void render(Map<String, Object> model, HttpServletRequest request, HttpServletResponse response) throws ServletException,IOException {
+        modelToRequestAttribute(model, request);
+        RequestDispatcher dispatcher = request.getRequestDispatcher(viewPath);
+        dispatcher.forward(request, response);
+
+
+    }
+
+    private void modelToRequestAttribute(Map<String, Object> model, HttpServletRequest request) {
+        model.forEach((key, value) -> request.setAttribute(key,value));
     }
 }
 
