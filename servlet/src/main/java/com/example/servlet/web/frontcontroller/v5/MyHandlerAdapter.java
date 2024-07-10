9306ff17ba0daef858efd83f87a4db626f0afffb
권길남 <nimpa3201@naver.com>
2024-07-10 21:17:13 +0900

    feat: MyHandlerAdapter 인터페이스 정의
    - 주어진 핸들러를 처리할 수 있는지 확인하는 supports 메서드 추가
    - 요청을 처리하고 ModelView를 반환하는 handle 메서드 추가

---
+++ servlet/src/main/java/com/example/servlet/web/frontcontroller/v5/MyHandlerAdapter.java
@@ -0,0 +1,14 @@
+package com.example.servlet.web.frontcontroller.v5;
+
+import com.example.servlet.web.frontcontroller.ModelView;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.web.client.HttpServerErrorException;
+
+import java.io.IOException;
+
+public interface MyHandlerAdapter {
+    boolean supports(Object handler);
+    ModelView handle(HttpServletRequest request, HttpServletResponse response, Object handler) throws HttpServerErrorException, IOException;
+
+}
