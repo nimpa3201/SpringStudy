793e0e2bc21f7f421a708670952093401b9691fb
권길남 <nimpa3201@naver.com>
2024-08-29 14:53:27 +0900

    Add: 레이아웃 메인 페이지 라우팅 추가
    - `/layout` 경로로 접근 시 `layoutMain` 페이지를 반환하는 메서드 추가
    - `TemplateController`에 레이아웃 관련 경로 설정

--- thymeleaf-basic/src/main/java/com/example/thymeleaf/basic/TemplateController.java
+++ thymeleaf-basic/src/main/java/com/example/thymeleaf/basic/TemplateController.java
@@ -14,5 +14,9 @@ public class TemplateController {
         return "template/fragment/fragmentMain";
     }
 
+    @GetMapping("layout")
+    public String layout(){
+        return "template/layout/layoutMain";
+    }
 
 }
