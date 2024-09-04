7eb8c75d0573ccb744a8b22db473616726d3fdc3
권길남 <nimpa3201@naver.com>
2024-09-04 11:37:36 +0900

    Feat: DeliveryCode 클래스 정의 및 ItemType ENUM 생성 (Lombok 어노테이션, 상품 설명 필드 추가)

---
+++ form/src/main/java/hello/itemservice/domain/item/ItemType.java
@@ -0,0 +1,12 @@
+package hello.itemservice.domain.item;
+
+public enum ItemType {
+    BOOK("도서") , FOOD("음식"), ETC("기타");
+    private final String description;
+
+    ItemType(String description) {
+        this.description = description;
+    }
+}
+//상품 종류는 `ENUM` 을 사용한다. 설명을 위해 `description` 필드를 추가했다.
+
