3d45d03e5b361e154efcac3c8ecb35f73545e0e6
권길남 <nimpa3201@naver.com>
2024-09-04 11:50:52 +0900

    Feat: DeliveryCode 클래스 정의 및 ItemType ENUM 생성 (Lombok 어노테이션, 상품 설명 필드 추가)

--- form/src/main/java/hello/itemservice/domain/item/DeliveryCode.java
+++ form/src/main/java/hello/itemservice/domain/item/DeliveryCode.java
@@ -1,2 +1,20 @@
-package hello.itemservice.domain.item;public class DeliveryCode {
+package hello.itemservice.domain.item;
+
+import lombok.Data;
+import lombok.RequiredArgsConstructor;
+
+
+/***
+ * Fast : 빠른 배송
+ * Normal : 일반배송
+ * Slow : 느린 배송
+ */
+@Data
+@RequiredArgsConstructor
+public class DeliveryCode {
+
+    private String code;
+    private String displayName;
+
+
 }
