9823b76de436d8c4a9c00e06643551c91bfdd990
권길남 <nimpa3201@naver.com>
2024-09-12 15:38:43 +0900

    Feat: 아이템 유효성 검사 로직을 별도의 ItemValidator 클래스로 분리
    - 기존 컨트롤러의 유효성 검사 로직을 추출하여 ItemValidator 클래스 생성
    - ItemValidator는 Validator 인터페이스를 구현
    - addItemV5 메서드에서 ItemValidator를 사용하여 유효성 검사 수행

--- validation/src/main/java/hello/itemservice/web/validation/ValidationItemControllerV2.java
+++ validation/src/main/java/hello/itemservice/web/validation/ValidationItemControllerV2.java
@@ -25,6 +25,7 @@ import java.util.Map;
 public class ValidationItemControllerV2 {
 
     private final ItemRepository itemRepository;
+    private final ItemValidator itemValidator;
 
     @GetMapping
     public String items(Model model) {
@@ -174,7 +175,7 @@ public class ValidationItemControllerV2 {
     }
 
 
-    @PostMapping("/add") //`BindingResult bindingResult` 파라미터의 위치는 `@ModelAttribute Item item` 다음에 와야 한다.
+    //@PostMapping("/add") //`BindingResult bindingResult` 파라미터의 위치는 `@ModelAttribute Item item` 다음에 와야 한다.
     public String addItemV4(@ModelAttribute Item item, BindingResult bindingResult ,RedirectAttributes redirectAttributes, Model model) {
         // bindReuslt = errors 와 같은 기능 ,검증 오류 결과를 보관
         log.info("objectName={}",bindingResult.getObjectName());
@@ -220,6 +221,29 @@ public class ValidationItemControllerV2 {
         return "redirect:/validation/v2/items/{itemId}";
     }
 
+
+
+
+    @PostMapping("/add") //`BindingResult bindingResult` 파라미터의 위치는 `@ModelAttribute Item item` 다음에 와야 한다.
+    public String addItemV5(@ModelAttribute Item item, BindingResult bindingResult ,RedirectAttributes redirectAttributes, Model model) {
+
+
+        itemValidator.validate(item,bindingResult);
+
+        // 검증에 실패하면 다른 입력 폼으로
+        if(bindingResult.hasErrors()){
+            log.info("error ={}", bindingResult);
+            //model.addAttribute("errors",errors); BindResult는 자동으로 view로 넘어감
+            return "/validation/v2/addForm";
+        }
+
+        // 성공 로직
+
+        Item savedItem = itemRepository.save(item);
+        redirectAttributes.addAttribute("itemId", savedItem.getId());
+        redirectAttributes.addAttribute("status", true);
+        return "redirect:/validation/v2/items/{itemId}";
+    }
 
     @GetMapping("/{itemId}/edit")
     public String editForm(@PathVariable Long itemId, Model model) {
