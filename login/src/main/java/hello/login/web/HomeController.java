a13fbee98b8a351c4b47c10897fa58e9314e201d
권길남 <nimpa3201@naver.com>
2024-09-19 11:59:05 +0900

    Feat: 스프링 세션 어노테이션을 활용한 로그인 처리 개선 (V3Spring)
    - homeLoginV3Spring 메서드 추가
    - @SessionAttribute 어노테이션을 사용하여 세션 데이터 접근
    - 로그인 여부에 따른 홈 화면 분기 로직 간소화
    
     스프링 프레임워크의 기능을 활용하여 코드 간소화 및 가독성 향상

--- login/src/main/java/hello/login/web/HomeController.java
+++ login/src/main/java/hello/login/web/HomeController.java
@@ -9,6 +9,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.CookieValue;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.SessionAttribute;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpSession;
@@ -58,7 +59,7 @@ public class HomeController {
 
 
 
-    @GetMapping("/") //required = false -> 로그인하지 않은 사용자도 허용(쿠키값 없는)
+    //@GetMapping("/") //required = false -> 로그인하지 않은 사용자도 허용(쿠키값 없는)
     public String homeLoginV3(HttpServletRequest request, Model model){
 
         HttpSession session = request.getSession(false);
@@ -67,6 +68,20 @@ public class HomeController {
         }
 
         Member loginMember = (Member)session.getAttribute(SessionConst.LOGIN_MEMBER);
+
+        if(loginMember == null){
+            return "home";
+        }
+        //로그인 성공한 사용자
+
+        model.addAttribute("member",loginMember);
+        return "loginHome";
+    }
+
+
+    @GetMapping("/") //required = false -> 로그인하지 않은 사용자도 허용(쿠키값 없는)
+    public String homeLoginV3Spring(@SessionAttribute(name = SessionConst.LOGIN_MEMBER,required = false) Member loginMember, Model model){
+
 
         if(loginMember == null){
             return "home";
