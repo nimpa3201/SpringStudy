ad7d20ba449f52b3e664a1919870eebe6bae6c4b
권길남 <nimpa3201@naver.com>
2024-09-27 22:31:02 +0900

    Feat: 커스텀 에러 페이지 처리 구현
    - ErrorPageController 추가: 404, 500 에러에 대한 커스텀 페이지 매핑
    - WebServerCustomizer 추가: 에러 상태 및 예외에 따른 에러 페이지 설정
    - 404, 500 HTTP 상태 코드 및 RuntimeException에 대한 처리 구현

---
+++ exception /src/main/java/hello/exception/WebServerCustomizer.java
@@ -0,0 +1,24 @@
+package hello.exception;
+
+import org.springframework.boot.web.server.ConfigurableWebServerFactory;
+import org.springframework.boot.web.server.ErrorPage;
+import org.springframework.boot.web.server.WebServerFactoryCustomizer;
+import org.springframework.http.HttpStatus;
+import org.springframework.stereotype.Component;
+
+@Component
+public class WebServerCustomizer implements WebServerFactoryCustomizer<ConfigurableWebServerFactory> {
+    @Override
+    public void customize(ConfigurableWebServerFactory factory) {
+
+        ErrorPage errorPage404 = new ErrorPage(HttpStatus.NOT_FOUND, "/error-page/404");
+        ErrorPage errorPage500 = new ErrorPage(HttpStatus.INTERNAL_SERVER_ERROR, "/error-page/500");
+
+        ErrorPage errorPageEx = new ErrorPage(RuntimeException.class , "/error-page/500");
+        //해당 예외와 자식 타입의 오류 함게 처리해줌
+
+        factory.addErrorPages(errorPage404,errorPage500,errorPageEx);
+
+
+    }
+}
