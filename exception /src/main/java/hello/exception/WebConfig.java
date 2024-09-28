e74ecf3bb4728e9f6cf0d20b39e000bc522a287c
권길남 <nimpa3201@naver.com>
2024-09-28 22:31:32 +0900

    Feat: 요청 및 응답 로깅을 위한 LogInterceptor 추가 및 설정
    - LogInterceptor를 사용해 HTTP 요청의 preHandle, postHandle, afterCompletion 단계에서 로그를 남김
      - 요청 URI, DispatcherType, Handler 및 로그 ID(UUID) 기록
      - 예외 발생 시 afterCompletion에서 오류 로그 추가
    - WebConfig에서 LogInterceptor 등록 및 설정
      - 모든 경로("/**")에 대해 인터셉터 적용
      - CSS, 아이콘 파일, 오류 페이지 경로("/error", "/error-page/**")에 대해서는 인터셉터 제외

--- exception /src/main/java/hello/exception/WebConfig.java
+++ exception /src/main/java/hello/exception/WebConfig.java
@@ -1,17 +1,34 @@
 package hello.exception;
 
 import hello.exception.filter.LogFilter;
+import hello.exception.interceptor.LogInterceptor;
 import jakarta.servlet.DispatcherType;
 import jakarta.servlet.Filter;
 import org.springframework.boot.web.servlet.FilterRegistrationBean;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
 public class WebConfig implements WebMvcConfigurer {
 
-    @Bean
+    @Override
+    public void addInterceptors(InterceptorRegistry registry) {
+        registry.addInterceptor( new LogInterceptor())
+                .order(1)
+                .addPathPatterns("/**")
+                .excludePathPatterns("/css/**","*.ico","/error","/error-page/**"); //오류 페이지 하부 빼버림
+    }
+
+
+
+//1. WAS(/error-ex, dispatchType=REQUEST) -> 필터 -> 서블릿 -> 인터셉터 -> 컨트롤러
+//2. WAS(여기까지 전파) <- 필터 <- 서블릿 <- 인터셉터 <- 컨트롤러(예외발생)
+//3. WAS 오류 페이지 확인
+//4. WAS(/error-page/500, dispatchType=ERROR) -> 필터(x) -> 서블릿 -> 인터셉터(x) -> 컨트롤러(/error-page/500) -> View
+
+    //@Bean
     public FilterRegistrationBean logFilter(){
         FilterRegistrationBean<Filter> filterFilterRegistrationBean = new FilterRegistrationBean<>();
         filterFilterRegistrationBean.setFilter(new LogFilter());
