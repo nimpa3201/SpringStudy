5852844b3cc048cdeab5273fadbabaa4d1141185
권길남 <nimpa3201@naver.com>
2024-10-03 20:10:50 +0900

    Feat: IllegalArgumentException에 대한 400 에러 처리 및 MyHandlerExceptionResolver 등록
    - IllegalArgumentException 발생 시 400 에러 반환하도록 MyHandlerExceptionResolver 구현
    - WebConfig에 MyHandlerExceptionResolver 등록

--- exception /src/main/java/hello/exception/WebConfig.java
+++ exception /src/main/java/hello/exception/WebConfig.java
@@ -2,13 +2,17 @@ package hello.exception;
 
 import hello.exception.filter.LogFilter;
 import hello.exception.interceptor.LogInterceptor;
+import hello.exception.resolver.MyHandlerExceptionResolver;
 import jakarta.servlet.DispatcherType;
 import jakarta.servlet.Filter;
 import org.springframework.boot.web.servlet.FilterRegistrationBean;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.HandlerExceptionResolver;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+import java.util.List;
 
 @Configuration
 public class WebConfig implements WebMvcConfigurer {
@@ -21,9 +25,12 @@ public class WebConfig implements WebMvcConfigurer {
                 .excludePathPatterns("/css/**","*.ico","/error","/error-page/**"); //오류 페이지 하부 빼버림
     }
 
+    @Override
+    public void extendHandlerExceptionResolvers(List<HandlerExceptionResolver> resolvers) {
+        resolvers.add(new MyHandlerExceptionResolver()); // 핸들러 등록
+    }
 
-
-//1. WAS(/error-ex, dispatchType=REQUEST) -> 필터 -> 서블릿 -> 인터셉터 -> 컨트롤러
+    //1. WAS(/error-ex, dispatchType=REQUEST) -> 필터 -> 서블릿 -> 인터셉터 -> 컨트롤러
 //2. WAS(여기까지 전파) <- 필터 <- 서블릿 <- 인터셉터 <- 컨트롤러(예외발생)
 //3. WAS 오류 페이지 확인
 //4. WAS(/error-page/500, dispatchType=ERROR) -> 필터(x) -> 서블릿 -> 인터셉터(x) -> 컨트롤러(/error-page/500) -> View
