96ef9b7415dfa592519c6ffb51a10effd5fd1c69
권길남 <nimpa3201@naver.com>
2024-10-07 14:53:05 +0900

    Feat: 사용자 정의 예외 처리기 등록
    - UserHandlerExceptionResolver 클래스 추가: UserException을 처리하여 JSON 또는 HTML 형식으로 에러 응답 제공.
    - extendHandlerExceptionResolvers 메서드 수정: UserHandlerExceptionResolver를 핸들러 목록에 등록.

--- exception /src/main/java/hello/exception/WebConfig.java
+++ exception /src/main/java/hello/exception/WebConfig.java
@@ -3,8 +3,10 @@ package hello.exception;
 import hello.exception.filter.LogFilter;
 import hello.exception.interceptor.LogInterceptor;
 import hello.exception.resolver.MyHandlerExceptionResolver;
+import hello.exception.resolver.UserHandlerExceptionResolver;
 import jakarta.servlet.DispatcherType;
 import jakarta.servlet.Filter;
+import org.apache.catalina.User;
 import org.springframework.boot.web.servlet.FilterRegistrationBean;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -27,8 +29,11 @@ public class WebConfig implements WebMvcConfigurer {
 
     @Override
     public void extendHandlerExceptionResolvers(List<HandlerExceptionResolver> resolvers) {
-        resolvers.add(new MyHandlerExceptionResolver()); // 핸들러 등록
+        resolvers.add(new MyHandlerExceptionResolver());// 핸들러 등록
+        resolvers.add(new UserHandlerExceptionResolver());
     }
+
+
 
     //1. WAS(/error-ex, dispatchType=REQUEST) -> 필터 -> 서블릿 -> 인터셉터 -> 컨트롤러
 //2. WAS(여기까지 전파) <- 필터 <- 서블릿 <- 인터셉터 <- 컨트롤러(예외발생)
