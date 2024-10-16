a840d8dcf990d9ed836274f0bb8edd60960dd646
권길남 <nimpa3201@naver.com>
2024-10-16 21:29:33 +0900

    Feat: WebConfig에 MyNumberFormatter 추가 및 우선순위로 인한 컨버터 주석 처리

--- typeconverter/src/main/java/hello/typeconverter/WebConfig.java
+++ typeconverter/src/main/java/hello/typeconverter/WebConfig.java
@@ -4,6 +4,7 @@ import hello.typeconverter.converter.IntegerToStringConverter;
 import hello.typeconverter.converter.IpPortToStringConverter;
 import hello.typeconverter.converter.StringToIntegerConverter;
 import hello.typeconverter.converter.StringToIpPortConverter;
+import hello.typeconverter.formatter.MyNumberFormatter;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.format.FormatterRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
@@ -13,7 +14,11 @@ public class WebConfig implements WebMvcConfigurer {
     public void addFormatters(FormatterRegistry registry) {
         registry.addConverter(new StringToIpPortConverter());
         registry.addConverter(new IpPortToStringConverter());
-        registry.addConverter(new StringToIntegerConverter());
-        registry.addConverter(new IntegerToStringConverter());
+        //registry.addConverter(new StringToIntegerConverter()); 우선순위 때문에 주석처리
+        //registry.addConverter(new IntegerToStringConverter());
+
+        //추가
+        registry.addFormatter(new MyNumberFormatter()); //컨버터가 우선순위 높음
+
     }
 }
